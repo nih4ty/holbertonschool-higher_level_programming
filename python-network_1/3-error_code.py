4105727fdbfbccb46b5a886d9d6582c8f6a221c9
nih4ty <nihatyolchiyev01@gmail.com>
2025-12-07 19:32:55 +0000

    salam

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and prints the body or the HTTP error code."""
-
+"""Fetches a URLprints the body or an error code."""
 import sys
-from urllib import request, error
-
-
-def fetch_url():
-    """Fetches URL and handles HTTPError, printing the response or error code."""
-    url = sys.argv[1]
-
-    req = request.Request(url)
-
-    try:
-        with request.urlopen(req) as response:
-            body = response.read()
-            print(body.decode("utf-8"))
-
-    except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+import urllib.request
+import urllib.error
 
 
 if __name__ == "__main__":
-    fetch_url()
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
