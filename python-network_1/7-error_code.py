5a1a69bf17df936d4d0c2a9008a6d71118901ba0
nih4ty <nihatyolchiyev01@gmail.com>
2025-12-07 19:29:41 +0000

    salam

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and prints body or error code."""
-
+"""Sends a GET request to a URL and prints the response body or error code."""
 import sys
 import requests
 
 
-def fetch_url():
-    """Fetches URL and prints response body or error code."""
+if __name__ == "__main__":
     url = sys.argv[1]
-
     response = requests.get(url)
-
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+        print("Error code:", response.status_code)
     else:
         print(response.text)
-
-
-if __name__ == "__main__":
-    fetch_url()
-
